810f2462ced07651412219ce041f714946b66992
miquelarrombatleS2P <miquelarrom@paucasesnovescifp.cat>
2020-11-06 15:16:57 +0100

    In this exercise we have seen some events of different components such as click, doubleclick, mouseenter, mouseleave, focus, leave among others.

---
+++ WindowsFormsCommonEvents/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsCommonEvents
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messagesTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // messagesTextBox
+            // 
+            this.messagesTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.messagesTextBox.ForeColor = System.Drawing.Color.White;
+            this.messagesTextBox.Location = new System.Drawing.Point(370, 30);
+            this.messagesTextBox.Multiline = true;
+            this.messagesTextBox.Name = "messagesTextBox";
+            this.messagesTextBox.ReadOnly = true;
+            this.messagesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.messagesTextBox.Size = new System.Drawing.Size(473, 431);
+            this.messagesTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(21, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Double Click Me!";
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(24, 78);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "First line",
+            "Second line",
+            "Third line"});
+            this.listBox1.Location = new System.Drawing.Point(24, 141);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 52);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsCommonEvents.Properties.Resources.img;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 170);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(864, 498);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messagesTextBox);
+            this.Name = "mainForm";
+            this.Text = "Windows Forms Components - Common Events";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainForm_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messagesTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsCommonEvents/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsCommonEvents
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void mainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            string text = null;
+            if (e.Button == MouseButtons.Left)
+            {
+                text += $"Left button of the mouse has been clicked over main form. ";
+            }
+            else
+            {
+                text += $"Right button of the mouse has been clicked over main form ";
+            }
+
+            messagesTextBox.AppendText(text + Environment.NewLine);
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("Main Form Loaded." + Environment.NewLine);
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("The System has detected a Double Click on the label"+Environment.NewLine);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("Numeric Up Down value changed " + numericUpDown1.Value + Environment.NewLine);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("List Box Selected Index has changed to "+listBox1.SelectedIndex+Environment.NewLine);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("textBox1 has got the focus now" + Environment.NewLine);
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("textBox1 has lost the focus" + Environment.NewLine);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            messagesTextBox.AppendText($"The key '{textBox1.Text}' has been pressed and released." + Environment.NewLine);
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("The mouse pointer is inside the image" + Environment.NewLine);
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            messagesTextBox.AppendText("The mouse pointer is outside the image" + Environment.NewLine);
+        }
+    }
+}
